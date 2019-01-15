4b3d764da4e614865e64d5d49b15edaa92a4e02c
Robert S <rsavian@nerdery.com>
2019-01-15 16:33:01 -0600

    update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,9 @@
 // https://github.com/kulshekhar/ts-jest
 
 module.exports = {
+    verbose: false,
     preset: 'ts-jest',
     testEnvironment: 'node',
     roots: ['<rootDir>/src'],
+    collectCoverageFrom: ['src/**/*.ts', '!src/**/*.{test,spec,d}.ts'],
 };
